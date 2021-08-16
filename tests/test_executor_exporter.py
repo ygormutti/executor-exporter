096592e3f1eb924968a3762329bb2dfc73a2d263
Ygor Mutti <ygormutti@gmail.com>
2021-08-16 16:39:42 -0300

    WIP tests

--- tests/test_executor_exporter.py
+++ tests/test_executor_exporter.py
@@ -1,5 +1,24 @@
-from executor_exporter import __version__
+from concurrent.futures import Executor
+
+from prometheus_client import CollectorRegistry, generate_latest
+
+from executor_exporter import __version__, ExecutorExporter
+from executor_exporter.exporter import Metrics
 
 
 def test_version():
     assert __version__ == "0.1.0"
+
+
+def test_executor_exporter(snapshot):
+    registry = CollectorRegistry()
+    metrics = Metrics(registry)
+    exporter = ExecutorExporter(DummyExecutor(), "test-executor-id", metrics)
+    
+    exporter.inc_max_workers
+
+    snapshot.assert_match(generate_latest(registry))
+
+
+class DummyExecutor(Executor):
+    pass
